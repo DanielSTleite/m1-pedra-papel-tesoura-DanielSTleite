93df52a7e8ef891026a0ab5375282cc0a499f46b
Daniel <danielstleite@gmail.com>
2024-03-01 14:04:21 -0300

    Correção de redundâncias

--- script.js
+++ script.js
@@ -1,58 +1,20 @@
 /* Desenvolva seu código abaixo */
 function playRockPaperScissor(player1, player2) {
-   // Pedra
-   if (player1 == "Pedra" && player2 == "Pedra") {
-      let player1 = "Pedra";
-      let player2 = "Pedra";
+
+   if (player1 == player2) {
       let expectedResult = "Empate!";
       return expectedResult;
    }
-   else if (player1 == "Pedra" && player2 == "Tesoura") {
-      let player1 = "Pedra";
-      let player2 = "Tesoura";
+
+   else if ((player1 == "Pedra" && player2 == "Tesoura") || (player1 == "Papel" && player2 == "Pedra") || (player1 == "Tesoura" && player2 == "Papel")) {
       let expectedResult = "Jogador 1 venceu!";
       return expectedResult;
-   } else if (player1 == "Pedra" && player2 == "Papel") {
-      let player1 = "Pedra";
-      let player2 = "Papel";
-      let expectedResult = "Jogador 2 venceu!";
-      return expectedResult;
-   } else if (player1 == "Papel" && player2 == "Papel")   // Papel
-   {
-      let player1 = "Papel";
-      let player2 = "Papel";
-      let expectedResult = "Empate!";
-      return expectedResult;
-   } else if (player1 == "Papel" && player2 == "Pedra") {
-      let player1 = "Papel";
-      let player2 = "Pedra";
-      let expectedResult = "Jogador 1 venceu!";
-      return expectedResult;
-   } else if (player1 == "Papel" && player2 == "Tesoura") {
-      let player1 = "Papel";
-      let player2 = "Tesoura";
-      let expectedResult = "Jogador 2 venceu!";
-      return expectedResult;
-   } else if (player1 == "Tesoura" && player2 == "Tesoura")   // Tesoura
-   {
-      let player1 = "Tesoura";
-      let player2 = "Tesoura";
-      let expectedResult = "Empate!";
-      return expectedResult;
-   } else if (player1 == "Tesoura" && player2 == "Papel") {
-      let player1 = "Tesoura";
-      let player2 = "Papel";
-      let expectedResult = "Jogador 1 venceu!";
-      return expectedResult;
-   } else if (player1 == "Tesoura" && player2 == "Pedra") {
-      let player1 = "Tesoura";
-      let player2 = "Pedra";
+   } else if ((player1 == "Pedra" && player2 == "Papel") || (player1 == "Papel" && player2 == "Tesoura") || (player1 == "Tesoura" && player2 == "Pedra")) {
       let expectedResult = "Jogador 2 venceu!";
       return expectedResult;
    }
-};
 
-
-const resultado = playRockPaperScissor("Tesoura", "Tesoura");
-console.log(resultado); // Saída: "Jogador 1 venceu!"
+}
+const resultado = playRockPaperScissor("", "");
+console.log(resultado);
 
